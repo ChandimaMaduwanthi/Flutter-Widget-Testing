c2e6a3c2aa2ce70a759307cee9bd33a21d8931c0
ChandimaMaduwanthi <48376228+ChandimaMaduwanthi@users.noreply.github.com>
2021-01-17 19:39:54 +0530

    device_preview added

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:device_preview/device_preview.dart';
 void main() {
   runApp(
     DevicePreview(
-    builder: (context) => MyApp(), // Wrap your app
+    builder: (context) => MyApp(),
   ),
   );
 }
